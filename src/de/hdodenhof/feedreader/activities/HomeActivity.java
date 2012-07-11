a49c57be9b22dc0a0a235899c592b460bc77d703
Henning Dodenhof <henning.dodenhof@gmail.com>
2012-07-11 23:56:27 +0200

    Use XMLPullParser instead of SAXParser for article parsing - still needs some work

--- src/de/hdodenhof/feedreader/activities/HomeActivity.java
+++ src/de/hdodenhof/feedreader/activities/HomeActivity.java
@@ -3,8 +3,12 @@ package de.hdodenhof.feedreader.activities;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,6 +16,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.AlertDialog;
@@ -28,6 +33,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -43,9 +49,6 @@ import de.hdodenhof.feedreader.listadapters.RSSAdapter;
 import de.hdodenhof.feedreader.listadapters.RSSArticleAdapter;
 import de.hdodenhof.feedreader.listadapters.RSSFeedAdapter;
 import de.hdodenhof.feedreader.misc.FragmentCallback;
-import de.hdodenhof.feedreader.models.Article;
-import de.hdodenhof.feedreader.parser.ArticleHandler;
-import de.hdodenhof.feedreader.parser.SAXHelper;
 import de.hdodenhof.feedreader.provider.RSSContentProvider;
 import de.hdodenhof.feedreader.provider.SQLiteHelper;
 import de.hdodenhof.feedreader.provider.SQLiteHelper.ArticleDAO;
@@ -312,8 +315,8 @@ public class HomeActivity extends FragmentActivity implements FragmentCallback, 
                         String mName = "";
 
                         try {
-                                boolean mIsTitle = false; 
-                                
+                                boolean mIsTitle = false;
+
                                 InputStream mInputStream = new URL(mURL).openConnection().getInputStream();
 
                                 XmlPullParserFactory mParserFactory = XmlPullParserFactory.newInstance();
@@ -322,7 +325,7 @@ public class HomeActivity extends FragmentActivity implements FragmentCallback, 
                                 mPullParser.setInput(mInputStream, null);
 
                                 int mEventType = mPullParser.getEventType();
-                                
+
                                 while (mEventType != XmlPullParser.END_DOCUMENT) {
                                         if (mEventType == XmlPullParser.START_TAG) {
                                                 if (mPullParser.getName().equalsIgnoreCase("title")) {
@@ -387,70 +390,146 @@ public class HomeActivity extends FragmentActivity implements FragmentCallback, 
                         this.mContext = context;
                 }
 
-                @SuppressWarnings("unchecked")
                 protected Void doInBackground(Void... params) {
 
-                        ArrayList<Article> mArticles = new ArrayList<Article>();
+                        final String DATE_FORMATS[] = { "EEE, dd MMM yyyy HH:mm:ss Z", "EEE, dd MMM yyyy HH:mm:ss z", "yyyy-MM-dd'T'HH:mm:ssz",
+                                        "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSSZ" };
+                        SimpleDateFormat mSimpleDateFormats[] = new SimpleDateFormat[DATE_FORMATS.length];
+
+                        for (int i = 0; i < DATE_FORMATS.length; i++) {
+                                mSimpleDateFormats[i] = new SimpleDateFormat(DATE_FORMATS[i], Locale.US);
+                                mSimpleDateFormats[i].setTimeZone(TimeZone.getTimeZone("GMT"));
+                        }
 
                         ContentResolver mContentResolver = mContext.getContentResolver();
 
                         try {
                                 String[] mProjection = { FeedDAO._ID, FeedDAO.NAME, FeedDAO.URL };
                                 Cursor mCursor = mContentResolver.query(RSSContentProvider.URI_FEEDS, mProjection, null, null, null);
+
+                                ArrayList<String[]> mFeeds = new ArrayList<String[]>();
+
                                 mCursor.moveToFirst();
-
                                 do {
-                                        int mFeedID = mCursor.getInt(mCursor.getColumnIndex(FeedDAO._ID));
-                                        SAXHelper mSAXHelper = new SAXHelper(mCursor.getString(mCursor.getColumnIndex(FeedDAO.URL)), new ArticleHandler());
-                                        mArticles = (ArrayList<Article>) mSAXHelper.parse();
-                                        int i = 0;
+                                        mFeeds.add(new String[] { mCursor.getString(mCursor.getColumnIndex(FeedDAO._ID)),
+                                                        mCursor.getString(mCursor.getColumnIndex(FeedDAO.URL)) });
+                                } while (mCursor.moveToNext());
 
-                                        ContentValues[] mContentValuesArray = new ContentValues[mArticles.size()];
-                                        for (Article mArticle : mArticles) {
+                                boolean mIsArticle = false;
 
-                                                mArticle.setFeedId(mFeedID);
+                                XmlPullParserFactory mParserFactory = XmlPullParserFactory.newInstance();
+                                mParserFactory.setNamespaceAware(true);
 
-                                                Document mDocument = Jsoup.parse(mArticle.getContent());
-                                                Elements mIframes = mDocument.getElementsByTag("iframe");
+                                int i = 1;
+                                for (String[] mFeed : mFeeds) {
+                                        ArrayList<ContentValues> mContentValuesArrayList = new ArrayList<ContentValues>();
 
-                                                TextNode mPlaceholder = new TextNode("(video removed)", null);
-                                                for (Element mIframe : mIframes) {
-                                                        mIframe.replaceWith(mPlaceholder);
-                                                }
+                                        InputStream mInputStream = new URL(mFeed[1]).openConnection().getInputStream();
 
-                                                mArticle.setContent(mDocument.html());
+                                        XmlPullParser mPullParser = mParserFactory.newPullParser();
+                                        mPullParser.setInput(mInputStream, null);
 
-                                                if (mArticle.getSummary() == null) {
-                                                        String mSummary = mDocument.text();
-                                                        if (mSummary.length() > SUMMARY_MAXLENGTH) {
-                                                                mArticle.setSummary(mSummary.substring(0, SUMMARY_MAXLENGTH));
-                                                        } else {
-                                                                mArticle.setSummary(mSummary);
+                                        String mCurrentTag;
+
+                                        String mTitle = null;
+                                        String mSummary = null;
+                                        String mContent = null;
+                                        String mGUID = null;
+                                        String mPubdate = null;
+
+                                        int mEventType = mPullParser.getEventType();
+
+                                        while (mEventType != XmlPullParser.END_DOCUMENT) {
+                                                if (mEventType == XmlPullParser.START_TAG) {
+                                                        mCurrentTag = mPullParser.getName();
+                                                        if (mCurrentTag.equalsIgnoreCase("item") || mCurrentTag.equalsIgnoreCase("entry")) {
+                                                                mIsArticle = true;
+                                                        } else if (mCurrentTag.equalsIgnoreCase("title") && mIsArticle == true) {
+                                                                mTitle = mPullParser.nextText();
+                                                        } else if ((mCurrentTag.equalsIgnoreCase("description") || mCurrentTag.equalsIgnoreCase("summary"))
+                                                                        && mIsArticle == true) {
+                                                                mSummary = Html.fromHtml(mPullParser.nextText()).toString();
+                                                        } else if ((mCurrentTag.equalsIgnoreCase("encoded") || mCurrentTag.equalsIgnoreCase("content"))
+                                                                        && mIsArticle == true) {
+                                                                mContent = mPullParser.nextText();
+                                                        } else if ((mCurrentTag.equalsIgnoreCase("guid") || mCurrentTag.equalsIgnoreCase("id"))
+                                                                        && mIsArticle == true) {
+                                                                mGUID = mPullParser.nextText();
+                                                        } else if (mCurrentTag.equalsIgnoreCase("pubdate") || mCurrentTag.equalsIgnoreCase("published")
+                                                                        || mCurrentTag.equalsIgnoreCase("date")) {
+
+                                                                String mPubdateRaw = mPullParser.nextText();
+
+                                                                for (int j = 0; j < DATE_FORMATS.length; j++) {
+                                                                        try {
+                                                                                mPubdate = SQLiteHelper.fromDate((mSimpleDateFormats[j].parse(mPubdateRaw)));
+                                                                                break;
+                                                                        } catch (ParseException mParserException) {
+                                                                                if (j == DATE_FORMATS.length - 1) {
+                                                                                        throw new XmlPullParserException(mParserException.getMessage());
+                                                                                }
+                                                                        }
+                                                                }
                                                         }
+
+                                                } else if (mEventType == XmlPullParser.END_TAG) {
+                                                        mCurrentTag = mPullParser.getName();
+                                                        if (mCurrentTag.equalsIgnoreCase("item") || mCurrentTag.equalsIgnoreCase("entry")) {
+                                                                mIsArticle = false;
+
+                                                                Document mDocument = Jsoup.parse(mContent);
+
+                                                                Elements mIframes = mDocument.getElementsByTag("iframe");
+                                                                TextNode mPlaceholder = new TextNode("(video removed)", null);
+                                                                for (Element mIframe : mIframes) {
+                                                                        mIframe.replaceWith(mPlaceholder);
+                                                                }
+                                                                mContent = mDocument.html();
+
+                                                                if (mSummary == null) {
+                                                                        String mContentSummary = mDocument.text();
+                                                                        if (mContentSummary.length() > SUMMARY_MAXLENGTH) {
+                                                                                mSummary = mContentSummary.substring(0, SUMMARY_MAXLENGTH);
+                                                                        } else {
+                                                                                mSummary = mContentSummary;
+                                                                        }
+                                                                }
+
+                                                                ContentValues mContentValues = new ContentValues();
+
+                                                                mContentValues.put(ArticleDAO.FEEDID, mFeed[0]);
+                                                                mContentValues.put(ArticleDAO.GUID, mGUID);
+                                                                mContentValues.put(ArticleDAO.PUBDATE, mPubdate);
+                                                                mContentValues.put(ArticleDAO.TITLE, mTitle);
+                                                                mContentValues.put(ArticleDAO.SUMMARY, mSummary);
+                                                                mContentValues.put(ArticleDAO.CONTENT, mContent);
+                                                                mContentValues.put(ArticleDAO.READ, 0);
+
+                                                                mContentValuesArrayList.add(mContentValues);
+
+                                                                mTitle = null;
+                                                                mSummary = null;
+                                                                mContent = null;
+                                                                mGUID = null;
+                                                                mPubdate = null;
+                                                        }
+
                                                 }
-
-                                                ContentValues mContentValues = new ContentValues();
-
-                                                mContentValues.put(ArticleDAO.FEEDID, mArticle.getFeedId());
-                                                mContentValues.put(ArticleDAO.GUID, mArticle.getGuid());
-                                                mContentValues.put(ArticleDAO.PUBDATE, SQLiteHelper.fromDate(mArticle.getPubDate()));
-                                                mContentValues.put(ArticleDAO.TITLE, mArticle.getTitle());
-                                                mContentValues.put(ArticleDAO.SUMMARY, mArticle.getSummary());
-                                                mContentValues.put(ArticleDAO.CONTENT, mArticle.getContent());
-                                                mContentValues.put(ArticleDAO.READ, SQLiteHelper.fromBoolean(mArticle.isRead()));
-
-                                                mContentValuesArray[i++] = mContentValues;
+                                                mEventType = mPullParser.next();
 
                                         }
+                                        mInputStream.close();
 
-                                        mContentResolver.delete(RSSContentProvider.URI_ARTICLES, ArticleDAO.FEEDID + "=?",
-                                                        new String[] { Integer.toString(mFeedID) });
+                                        ContentValues[] mContentValuesArray = new ContentValues[mContentValuesArrayList.size()];
+                                        mContentValuesArray = mContentValuesArrayList.toArray(mContentValuesArray);
+
+                                        mContentResolver.delete(RSSContentProvider.URI_ARTICLES, ArticleDAO.FEEDID + "=?", new String[] { mFeed[0] });
                                         mContentResolver.bulkInsert(RSSContentProvider.URI_ARTICLES, mContentValuesArray);
 
                                         // TODO: update instead of insert if applicable, set lastUpdate on Feed
 
-                                        publishProgress(mCursor.getPosition() + 1);
-                                } while (mCursor.moveToNext());
+                                        publishProgress(i++);
+                                }
 
                         } catch (Exception e) {
                                 e.printStackTrace();

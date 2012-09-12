17857c0bb35803ecf1bc038e72e92961de2e364f
Henning Dodenhof <henning.dodenhof@gmail.com>
2012-09-12 17:16:37 +0200

    Fix for last update time in feed list

--- src/de/hdodenhof/feedreader/misc/Helpers.java
+++ src/de/hdodenhof/feedreader/misc/Helpers.java
@@ -79,14 +79,14 @@ public class Helpers {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
 
-        SimpleDateFormat timeFormatter = new SimpleDateFormat("kk:mm");
+        SimpleDateFormat timeFormatter = new SimpleDateFormat("HH:mm");
 
         if (calendar.get(Calendar.YEAR) == today.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == today.get(Calendar.DAY_OF_YEAR)) {
             return "Today, " + timeFormatter.format(date);
         } else if (calendar.get(Calendar.YEAR) == yesterday.get(Calendar.YEAR) && calendar.get(Calendar.DAY_OF_YEAR) == yesterday.get(Calendar.DAY_OF_YEAR)) {
             return "Yesterday, " + timeFormatter.format(date);
         } else {
-            return DateFormat.format("MMM dd, kk:mm", date).toString();
+            return DateFormat.format("MMM dd, HH:mm", date).toString();
         }
     }
 
